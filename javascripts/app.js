7adf5f8c4d34861ebcbda9630f9f5a35a41f6a62
srdjanpetrovic87 <srdjanpetrovic87@gmail.com>
2015-11-23 15:10:45 +0100

    Add smooth scroll funcionality

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,42 @@
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
+
+  $(document).on("scroll", onScroll);
+    //smoothscroll
+    $('.navbar-nav a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+      
+      $('a').each(function () {
+          $(this).parent().removeClass('active');
+      })
+
+      $(this).parent().addClass('active');
+    
+      var target = this.hash;
+      var menu = target;
+
+      $target = $(target);
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top-95
+      }, 750, 'swing', function () {
+          window.location.hash = target;
+          $(document).on("scroll", onScroll);
+      });
+    });
 });
+
+function onScroll(event){
+  var scrollPos = $(document).scrollTop();
+  $('.navbar-nav a').each(function () {
+      var currLink = $(this);
+      var refElement = $(currLink.attr("href"));
+      if (refElement.position().top-95 <= scrollPos && refElement.position().top + refElement.outerHeight() > scrollPos) {
+        $('.navbar-nav li').removeClass("active");
+        currLink.parent().addClass("active");
+      }
+      else{
+        currLink.parent().removeClass("active");
+      }
+  });
+}
